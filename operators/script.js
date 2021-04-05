a73949c8942a8b367638c39aecae1af1da883647
alirezafazeli8 <alirezafazeli34@gmail.com>
2021-04-05 12:03:06 +0430

    Assigenment Operators

---
+++ operators/script.js
@@ -0,0 +1,57 @@
+// assignment operators
+
+// plus => +
+// my operators variable
+let numberOne = 20;
+let numberTwo = 342;
+
+//---------------------complete explanation about how can show ------------------------
+// we can plus in external variable 
+let plusTwoNumber = numberOne + numberTwo; 
+// or plus in console.log();
+console.log(numberOne + numberTwo + " plus in variable");
+
+// log in console
+console.log(plusTwoNumber);
+
+// ----------------------------------------------------
+
+// minus => -
+let minusOperator = numberTwo - numberOne; //342 - 20 = 322
+console.log(minusOperator);
+console.log(numberOne - numberTwo); // if write like this because i want show you we can write like this, now don't matter how you can show in console.
+
+// --------------------------------------------------------
+
+// multiply => *
+let showMultiply = numberOne * numberTwo;
+console.log(showMultiply);
+console.log(numberTwo * numberOne);
+
+// --------------------------------------------------------
+
+// divide => /
+
+let showDivide = numberTwo / numberOne;
+console.log(showDivide);
+console.log(numberTwo / numberOne);
+
+// --------------------------------------------------------
+
+// modulo => %
+
+let showModulo = numberOne % numberTwo;
+console.log(showModulo); 
+console.log(numberOne % numberTwo);
+
+// ----------------------------------------------------------
+
+// 	Exponentiation =>  ** - in persian we can say (be tavan resoondan.).
+let showExponentiation = numberTwo ** numberOne;
+console.log(showExponentiation);
+
+// -----------------------------------------------------------
+//increment 
+
+//decrement
+
