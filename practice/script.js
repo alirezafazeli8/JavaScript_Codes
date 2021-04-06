ee5ac9d1042df7cde25ad7856b8b7ca66dce3d59
alirezafazeli8 <alirezafazeli34@gmail.com>
2021-04-06 13:06:31 +0430

    just for practicing

---
+++ practice/script.js
@@ -0,0 +1,35 @@
+//  Number One
+// use personal property in variable
+// name => string
+let firstName = "alireza";
+//family => string
+let lastName = "fazeli";
+//age => number
+let age = 16;
+// country => in my pratice country cant never changes
+const country = "Iran";
+
+// use string interpolation and show data in variable
+// use escape notation
+let messageFromServer = `Your Name is : ${firstName},\n Your LastName Is ${lastName},\n Your Age Is ${age},\n and your Country Is: ${country}`;
+
+// log the message in console
+// console.log(messageFromServer);
+
+// Number Two
+const yourAge = 45;
+let newAge = yourAge;
+newAge = 78;
+console.log(yourAge, newAge);
+// so in fact i can't change the data in const so now my project is broken.
+
+
+
+
+
+
+
+
+
+
+
