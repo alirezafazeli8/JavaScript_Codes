9ad43bbab838a9a6e0ad8b1088257c8f60d58e37
alirezafazeli8 <alirezafazeli34@gmail.com>
2021-04-07 16:33:44 +0430

    make calculator and show result with js dom (This Is For Mentor) and show in html file

--- course_mentor_code/basics-01-starting-project/assets/scripts/script.js
+++ course_mentor_code/basics-01-starting-project/assets/scripts/script.js
@@ -1,1 +1,12 @@
-alert("Hello World !!!");
+// make function for (persian: baraye jam kardan XD).
+function sumNumber (num1, num2) {
+    const sum = num1 + num2;
+    return sum
+}
+
+// declaring variable for showing a operators in real
+let x;
+let y;
+
+// thats my calculators result
+outputResult(sumNumber(x = 47203749024, y = 743274027492), `${x} + ${y}`);
