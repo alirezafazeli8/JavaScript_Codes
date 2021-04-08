68f2c8062449f1596c08b6cb3f013f0d895b89e2
alirezafazeli8 <alirezafazeli34@gmail.com>
2021-04-08 17:21:09 +0430

    string & number mixin in js

---
+++ practice/mixin/script.js
@@ -0,0 +1,15 @@
+let x;
+let y;
+
+x = 3 + "3";
+x = 3 * "45";
+x = 12 / "2";
+x = 12 - "2";
+x = '12' - 12;
+x = 2 - 12;
+
+console.log(typeof(x));
+console.log(x);
+
+x = "alireza" + "fazeli";
+console.log(x);
