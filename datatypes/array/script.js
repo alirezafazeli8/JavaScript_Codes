bc23040bc1d444d9d62aa2a4189fc28a33bd18f1
alirezafazeli8 <alirezafazeli34@gmail.com>
2021-04-10 16:56:21 +0430

    More Operators like : += -= .... , ++ __

---
+++ datatypes/array/script.js
@@ -0,0 +1,17 @@
+// write array
+let carNames = ["Benz", "BMW", "lamborghini", "Pride"];
+
+console.log(carNames);
+
+// get value in array
+console.log(carNames[0]); // => benz
+
+console.log(carNames[3]); // => Pride
+
+// import value in array
+
+let lastName = "Fazeli";
+
+carNames.push(lastName); // last name data added to array
+
+console.log(carNames);
