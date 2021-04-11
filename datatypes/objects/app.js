2378c177172424dc986605dc718a38df324f1648
alirezafazeli8 <alirezafazeli34@gmail.com>
2021-04-11 10:00:11 +0430

    new datatype

---
+++ datatypes/objects/app.js
@@ -0,0 +1,19 @@
+const users = {
+    userName: "alireza",
+    city: "Tehran",
+    age: 16,
+};
+
+console.log(users);
+
+let showUserInfo = `
+    welcome to database. XD
+
+    username : ${users.userName}
+    city : ${users.city}
+    age : ${users.age}
+
+    Your Information So Cute
+`;
+
+console.log(showUserInfo);
