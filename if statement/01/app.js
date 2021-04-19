867cfa1ba2c00a77fe4746c8ba8bdf8ad2bbb3c5
alirezafazeli8 <alirezafazeli34@gmail.com>
2021-04-19 17:10:57 +0430

    new course detail

---
+++ if statement/01/app.js
@@ -0,0 +1,12 @@
+const userAge = 16;
+const userName = `alireza`;
+
+if (userAge >= 18) {
+    console.log(`hey ${userName} you can drive with your car 🚗`);
+} else if (userAge == 16) {
+    console.log(`hey kid ${userName} , i wanna kidding with you, go f your self 😡.`);
+} else {
+ console.log(`sorry ${userName}, you can't drive with your car 😑`);   
+};
+
+
