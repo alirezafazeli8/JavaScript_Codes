041bf7d4d40da1c2262906007543f5ac1f2bdcf1
Alireza Fazeli <alirezafazeli34@gmail.com>
2021-05-04 16:12:22 +0430

    solve the challenge

---
+++ fundamental_2/challenges/app.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+//  average calculator
+const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
+    return (scoreOne + scoreTwo + scoreThree) / 3
+};
+
+// store calculate data from dolphin and koalas
+const dolphinAverage = calcAverage(85, 54, 41);
+const koalasAverage = calcAverage(23, 34, 27);
+
+// check the winner 
+function checkWinner (avgDolphins, avgKoalas) {
+   if (avgDolphins >=  2 *  avgKoalas) {
+        return `Dolphin is Win (${avgDolphins} vs ${avgKoalas}) `
+   } else if  (avgDolphins >=  2 * avgKoalas) {
+        return `Koalas is Win (${avgKoalas} vs ${avgDolphins}) `
+   } else {
+       return `We Dont Have Winner`
+   }
+}
+
+// result of winner form averages variable
+console.log(checkWinner(dolphinAverage, koalasAverage));
+*/
