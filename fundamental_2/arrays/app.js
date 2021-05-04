c1ea6ebecccf887ed04d93928be1c1ffabf794ac
Alireza Fazeli <alirezafazeli34@gmail.com>
2021-05-04 15:52:17 +0430

    array topic code

---
+++ fundamental_2/arrays/app.js
@@ -0,0 +1,20 @@
+"use strict";
+/*
+let fullName;
+const tellMeYourName = function (firstName, lastName) {
+    fullName = [firstName, lastName];
+    return fullName;
+};
+
+console.log(tellMeYourName("jack", "nelson"));
+console.log(fullName[0], fullName[1]);
+
+
+let firstName = ["alireza", "fazeli"];
+
+console.log(firstName);
+
+firstName[0] = "ghasem"; // change data
+console.log(firstName);
+
+*/
