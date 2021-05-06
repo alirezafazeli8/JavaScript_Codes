53b3bef568dd3a88997c7321902316ffc1a72017
Alireza Fazeli <alirezafazeli34@gmail.com>
2021-05-06 14:39:54 +0430

    i solve the course assignment

---
+++ fundamental_2/assignment/app.js
@@ -0,0 +1,66 @@
+"use strict";
+/*
+//Functions
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its
+    capital city is ${capitalCity}`
+}
+
+const countryOne = describeCountry("iran", "85", "tehran");
+const countryTwo = describeCountry("USA", "400", "NewYork");
+const countryThree = describeCountry("Germany", "250", "Berlin");
+
+console.log(countryOne);
+console.log(countryTwo);
+console.log(countryThree);
+
+*/
+
+/*
+// Topic : Function Declarations vs. Expressions
+//declaration function
+function percentageOfWorld1(population) {
+    return ` ${ (population / 7900) * 100}of the world population`
+}
+
+let iran = percentageOfWorld1(85);
+let usa = percentageOfWorld1(300);
+let uk = percentageOfWorld1(500);
+
+console.log(iran);
+console.log(usa);
+console.log(uk);
+
+//function expression
+const percentageOfWorld2 = function (population) {
+    return ` ${ (population / 7900) * 100}of the world population`
+}
+
+iran = percentageOfWorld1(85);
+usa = percentageOfWorld1(300);
+uk = percentageOfWorld1(500);
+
+console.log(iran);
+console.log(usa);
+console.log(uk);
+
+// arrow function
+const percentageOfWorld3 = (population) => ` ${ (population / 7900) * 100}of the world population`;
+
+iran = percentageOfWorld1(85);
+usa = percentageOfWorld1(300);
+uk = percentageOfWorld1(500);
+
+console.log(iran);
+console.log(usa);
+console.log(uk);
+
+*/
+/*
+// Topic: Introduction to A
+const populations = [85, 500, 200, 341];
+const percentageOfWorld2 = function (population) {
+    return ` ${ (population / 7900) * 100}of the world population`
+}
+console.log(percentageOfWorld2(populations[0]));
+*/
