6aaf967a9d481fc105cd7d7e281bf9ef7b679477
Alireza Fazeli <alirezafazeli34@gmail.com>
2021-06-01 09:22:29 +0430

    pige game project ....

---
+++ practice/pigGame/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// get element
+
+const playerScore0 = document.getElementById("score--0");
+const playerScore1 = document.getElementById("score--1");
+
+const playerCurrentScore0 =  document.getElementById("current--0");
+const playerCurrentScore1 =  document.getElementById("current--0");
+
+// btn
+const newBtn =  document.querySelector(".btn--new");
+const rollDiceBtn =  document.querySelector(".btn--roll");
+const holdBtn =  document.querySelector(".btn--hold");
+
+// img dice
+const diceImg = document.querySelector(".dice");
