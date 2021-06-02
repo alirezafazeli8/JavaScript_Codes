9df3fefda0fc12962d38cacb069ab21149d4a5de
aliezafazeli8 <alirezafazeli34@gmail.com>
2021-06-02 08:32:02 +0430

    forward from my loptop to my pc

--- practice/pigGame/script.js
+++ practice/pigGame/script.js
@@ -15,3 +15,71 @@ const holdBtn =  document.querySelector(".btn--hold");
 
 // img dice
 const diceImg = document.querySelector(".dice");
+
+// store random number
+let randomNumber = 0;
+
+// activeplayer 
+let activePlayer = 0;
+
+//player active variable
+const currentActivePlayer = document.getElementById(`current--${activePlayer}`);
+
+// player score
+let playerScore = [0, 0];
+
+// reset player score textcontent
+playerScore0.textContent = 0;
+playerScore1.textContent = 0;
+
+// change player Active
+const changePlayerActive = () => {
+	if(activePlayer === 0) {
+		activePlayer = 1;
+	} else if (activePlayer === 1) {
+		activePlayer = 0;
+	}
+};
+
+
+// change player
+const changePlayer = () => {
+	//last player
+
+	document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");	
+
+	changePlayerActive();
+
+	// next player
+	document.querySelector(`.player--${activePlayer}`).classList.add("player--active");	
+
+};
+
+// show current user score
+const showCurrentScore = () => {
+	currentActivePlayer.textContent = playerScore[activePlayer];
+};
+
+// user roll dice button event
+
+rollDiceBtn.addEventListener("click", function () {
+	// genrate random number
+	randomNumber = Math.trunc(Math.random() * (7 - 1) + 1 );
+
+	// display random number
+	diceImg.classList.remove("hidden");
+	diceImg.src = (`dice-${randomNumber}.png`);
+
+	//condition 
+	if(randomNumber === 1) {
+		changePlayer();
+	} else {
+		playerScore[activePlayer] += randomNumber;
+		showCurrentScore();
+	}
+	
+});
+
+
+
+
