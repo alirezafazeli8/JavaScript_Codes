9ef0f51f45d8b15deae4a5b31dbedf2ed9d5fd47
alireza <alirezafazeli34@gmail.com>
2021-08-04 23:00:56 +0430

    map method codes

---
+++ working_with_arrays(array tools)/datattransforming/mapMethod.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// const numbers = [1, 2, 3, 4];
+
+// const newNumbers = numbers.map(function (num) {
+//   return num * 4;
+// });
+
+// console.log(newNumbers);
+
+// const newNumber2 = numbers.map((num) => num * 4);
+
+// console.log(newNumber2);
+
+const arr = [3, 4, 5, 6, 7, 4];
+// let newArr = [];
+
+// arr.forEach(function (item) {
+//   newArr.push(item * 3);
+// });
+
+// console.log(newArr);
+
+const newArr = arr.map(function (item) {
+  return item * 2;
+});
+
+console.log(newArr);
+
+newArr.forEach(function (item) {
+  console.log("this is my item ", item);
+});
