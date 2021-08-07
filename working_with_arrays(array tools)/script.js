f72b756367fd1ebf3019333140b0f6c8e5c0c29b
alireza <alirezafazeli34@gmail.com>
2021-08-07 16:36:12 +0430

    array tools practice

--- working_with_arrays(array tools)/script.js
+++ working_with_arrays(array tools)/script.js
@@ -45,7 +45,7 @@ console.log(letters);
 // STUB join
 // NOTE should store to variable
 console.log(arr.join("+"));
-*/
+
 
 // STUB forEach
 
@@ -75,3 +75,96 @@ arr.forEach(function (myValue, i) {
   console.log(i);
   //   break;
 });
+*/
+
+// const arrNames = ["alireza", "sara", "noroz", "yekta"];
+// const arrNumbers = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
+// const strangerNumber = [-2, -4, -5, 4, 1];
+
+// console.log();
+// arrNames.slice(2, 4);
+// console.log(arrNames);
+
+// console.log(arrNames.slice(2, 4));
+// arrNames.splice(2, 4);
+
+// console.log(arrNames);
+
+// arrNames.reverse();
+// console.log(arrNames);
+
+// const newArr = arrNames.concat(arrNumbers);
+// const arr = [...arrNumbers, ...arrNames];
+// console.log(newArr);
+// console.log(arr);
+
+// console.log(arrNames.join("|"));
+
+// console.log(strangerNumber);
+// console.log(Math.abs(strangerNumber));
+// strangerNumber.forEach(function (value) {
+//   console.log(Math.abs(value));
+// });
+
+// const newStrangerNumbers = strangerNumber.map(function (value) {
+//   return Math.abs(value);
+// });
+
+// console.log(newStrangerNumbers);
+
+// const thatsFor = strangerNumber.forEach(function (value, i, array) {
+//   console.log();
+// });
+
+// arrNames.forEach((value) => console.log(value));
+
+// console.log(thatsFor);
+
+// arrNames.forEach(function (value, i) {
+//   const htmlCode = `
+//     <li>${i + 1} ${value}</li>
+//   `;
+
+//   document.querySelector(".my_ul").insertAdjacentHTML("beforeend", htmlCode);
+// });
+
+// document.querySelector(".my_ul").innerHTML = "<li>hello</li>";
+
+// map
+// filter
+// reduce
+
+// const arrNames = ["alireza", "sara", "noroz", "yekta"];
+// let arrNumbers = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
+// const strangerNumber = [-2, -4, -5, 4, 1];
+
+// const newNames = arrNames.map(function (value, index, arr) {
+//   // return `${value} ${index} ${arr}`;
+//   return value + " T";
+// });
+
+// console.log(newNames);
+
+// const aFilter = arrNames.filter(function (value) {
+//   return value[value.lastIndexOf("a")] == "a";
+// });
+
+// console.log(aFilter);
+
+// function turnValue() {
+//   arrNumbers = arrNumbers.map((value) => value * 2);
+// }
+
+// turnValue();
+// console.log(arrNumbers);
+
+// let arrNumbers = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
+
+// const allNumbers = arrNumbers.reduce(function (last, current, i, array) {
+//   // return last * current;
+//   return `
+//     ${i} sum : ${last} ${current}.${array}
+//   `;
+// });
+
+// console.log(allNumbers);
