244c6b40ee2137589728b59b2825ad5a644fc4b9
alireza <alirezafazeli34@gmail.com>
2021-08-08 14:29:20 +0430

    filter method code and practice

---
+++ working_with_arrays(array tools)/datattransforming/filterMethod.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// NOTE mini challenge in course
+// const transforming = [200, -200, 340, -300, -20, 50, 400, -460];
+
+// const widthDraw = transforming.filter(function (money, i) {
+//   return money < 0;
+// });
+
+// console.log(widthDraw);
+
+/*
+const names = [
+  "alireza",
+  "arman",
+  "korosh",
+  "niayesh",
+  "reza",
+  "akbar",
+  "Akabara",
+  "Antalia",
+];
+
+const aNames = names.filter(function (n) {
+  return n[0] == "a" || n[0] == "A";
+});
+
+console.log(aNames);
+*/
