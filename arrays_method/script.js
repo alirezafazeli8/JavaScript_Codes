db51b75ceb94821cdbea93d8bfdc69b2062df4c0
alireza <alirezafazeli34@gmail.com>
2021-09-14 22:01:38 +0430

    arrays method js new thing is added

--- arrays_method/script.js
+++ arrays_method/script.js
@@ -1,1 +1,36 @@
 "use strict";
+
+// const moneys = [100, -33, 34, -2123, -4232, 44234];
+
+// const { raft, amad } = moneys.reduce(
+//   function (l, f) {
+//     l[f > 0 ? "raft" : "amad"] += f;
+//     return l;
+//   },
+//   { raft: 0, amad: 0 }
+// );
+
+// console.log(raft, amad);
+
+// convert title case function
+
+// const convertTitle = function (title) {
+//   // unknown array
+//   const unknownWord = ["and", "a", "or", "then"];
+//   // title word array
+//   let word = title.trim().split(" ");
+
+//   //   return word.filter(function (cWord) {
+//   //     unknownWord.forEach(function (unWord) {
+//   //       return cWord != unWord;
+//   //     });
+//   //   });
+// };
+
+// console.log(convertTitle("hello my name is alireza and i wanna a or then now"));
+
+// const randomInt = function (min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+
+// console.log(randomInt(3, 5));
