dd4c6e7210171656835b5cbb94021f4258532cc7
alireza <alirezafazeli34@gmail.com>
2021-10-05 16:25:25 +0330

    practice is compeleted and new thing for learning is added

--- practice/00710/app.js
+++ practice/00710/app.js
@@ -343,8 +343,143 @@ const numbers = [4, 2, 6, 54, 2, 8];
 
 // console.log(nestedArr);
 
-const fuckingArr = new Array(3);
+// const fuckingArr = new Array(3);
 
-fuckingArr.fill("2");
+// fuckingArr.fill("2");
 
-console.log(fuckingArr);
+// console.log(fuckingArr);
+
+// console.log(+"1" + 1);
+
+const sNumber = "43dasdasdasd";
+const sNumber2 = "daasdasdsa432432432443";
+const sFloat = "43.4454dasdasdasd";
+const sFloat2 = "daasdasdsa432432432443.1548645";
+const realNumber = 15645;
+
+// console.log(parseInt(sNumber));
+// console.log(parseInt(sNumber2)); // NaN
+// console.log(parseFloat(sFloat));
+// console.log(parseFloat(sFloat2));
+
+// console.log(Number.isInteger(realNumber));
+// console.log(Number.isInteger(sNumber));
+
+// console.log(Math.sqrt(10));
+
+// const arrNumber = [1, 2, 5, 9, 2, 4, 5];
+
+// console.log(Math.max(15, 2, 81, 4, 94, 4));
+// console.log(Math.min(15, 2, 81, 4, 94, 4));
+// const PI = Math.PI;
+
+// console.log(PI);
+// console.log(Math.trunc(Math.random() * (6 + 1 - 2) + 2));
+
+// console.log(Math.round(56.4));
+// console.log(Math.ceil(56.1));
+// console.log(Math.floor(56.9));
+// console.log((4.656564454545).toFixed(5));
+// console.log(57 % 2);
+
+// console.log(
+//   typeof 54653435454544444444444444444444444444444444444444444444444444444444444444444444444444n
+// );
+
+// const bI = BigInt(455555555555555555555555555555555555555555555555555555555555);
+
+// console.log(bI);
+
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMonth());
+// console.log(myDate.getDate());
+// console.log(myDate.getDay());
+// console.log(myDate.getHours());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getSeconds());
+
+// console.log(myDate.toISOString());
+
+// console.log(myDate.getTime());
+// console.log(new Date(1633416776724));
+
+// console.log(Date.now() / (1000 * 60 * 60 * 24) / 365);
+
+// console.log(myDate.getTime());
+
+// if (cToDay > 0) {
+// } else {
+//   console.log("today");
+// }
+
+// console.log(cToDay);
+
+// // your passed date.
+// const myDate = new Date("2021 oct 1");
+
+// // now date
+// const nowDate = new Date();
+
+// //  get correct millisecond passes
+// const cToDay = myDate.getTime() - Date.now();
+
+// // clean the number
+// const clearNumber = Math.abs(Math.trunc(cToDay / (1000 * 60 * 60 * 24)));
+
+// console.log(clearNumber); // output => 4
+
+// const nowDate = new Date();
+
+// const myIntl = new Intl.DateTimeFormat("ira", {
+//   year: "numeric",
+//   month: "numeric",
+//   day: "2-digit",
+//   weekday: "long",
+// }).format(nowDate);
+
+// console.log(myIntl);
+
+// const numFormat = new Intl.NumberFormat("fa-ir", {
+//   style: "unit",
+//   unit: "inch",
+// }).format(54545);
+
+// console.log(numFormat);
+
+// const myTimeOut = setTimeout(
+//   function (num) {
+//     console.log(num);
+//   },
+//   1000,
+//   "hello"
+// );
+
+// clearTimeout(myTimeOut);
+const container = document.querySelector(".container");
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.getElementsByTagName("head"));
+// console.log(document.body.parentElement);
+
+const pEl = document.createElement("p");
+pEl.innerHTML = "<span>Hello</span>";
+container.append(pEl);
+
+console.log(pEl);
+const copyClone = pEl.cloneNode();
+copyClone.innerText = "hello";
+console.log(copyClone);
+
+// pEl.remove();
+
+// container.removeChild("");
+
+// console.log(getComputedStyle(pEl));
+
+container.setAttribute("data-password", "1234");
+// pEl.setAttribute("data", "user-name");
+
+// console.log(pEl.setAttributeNod));
+
+console.log(container.dataset);
