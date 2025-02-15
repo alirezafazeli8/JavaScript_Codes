04d0fb0dae5a8e657b15ad70eb485622dd8faca8
alirezafazeli8 <alirezafazeli34@gmail.com>
2025-02-15 17:59:16 +0330

    data types in js concepts

--- practice/1403_start_again/variable_data_type/app.js
+++ practice/1403_start_again/variable_data_type/app.js
@@ -1,20 +1,60 @@
 "use strict";
-console.group("All Things  :");
-console.log("Hello, world");
 
-console.error("You have error");
+// ----- work with console
 
-console.warn("Becareful dear developer");
+// console.group("All Things  :");
+// console.log("Hello, world");
 
-const userData = {
-	name: "alireza",
-	lastName: "fazeli",
-	gmail: "test@test.com",
-};
+// console.error("You have error");
 
-console.table(userData);
-console.groupEnd();
+// console.warn("Becareful dear developer");
 
-// console css style
-const consoleStyle = "background-color : yellow; color: red; padding: 20px";
-console.log("%cWelcome", consoleStyle);
+// const userData = {
+// 	name: "alireza",
+// 	lastName: "fazeli",
+// 	gmail: "test@test.com",
+// };
+
+// console.table(userData);
+// console.groupEnd();
+
+// // console css style
+// const consoleStyle = "background-color : yellow; color: red; padding: 20px";
+// console.log("%cWelcome", consoleStyle);
+
+//
+
+// const a = 0,
+// 	b = 0,
+// 	c = 0;
+
+// let d, e, f;
+
+// let firstName = null;
+// let userData = {};
+
+// console.log(firstName);
+// console.log(userData);
+// // console.log(userName);
+
+// console.log(typeof firstName);
+
+// if (typeof firstName == "string") {
+// 	console.log("This is string");
+// } else {
+// 	console.log("this is not string");
+// }
+
+// let und = undefined;
+
+// console.log(typeof und);
+
+// let iranSymbol = Symbol("iran flag");
+// console.log(typeof iranSymbol);
+
+// let name = "Jon";
+// let newName = name;
+// newName = "Johanatan";
+
+// console.log(name);
+// console.log(newName);
